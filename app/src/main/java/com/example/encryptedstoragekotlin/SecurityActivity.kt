8cd5a532388fd2e3884089d276cbd691ab07903c
micha <michael.azogu>
2020-05-14 17:35:02 -0400

    Started new project.

---
+++ app/src/main/java/com/example/encryptedstoragekotlin/SecurityActivity.kt
@@ -0,0 +1,12 @@
+package com.example.encryptedstoragekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecurityActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
